24647093cda107f99260417f871a6992d7702cbd
Quang T Nguyen <129918147+quangng2000@users.noreply.github.com>
2026-08-05 05:33:40 -0700

    Revert "Paper 1: ModelSMC reading notes"

--- papers/01-modelsmc/program-synthesis-prototype/src/shell/config/index.ts
+++
@@ -1,5 +0,0 @@
-export { ConfigurationError } from "./errors.js";
-export { parseExperimentConfig } from "./parse.js";
-export { withConfigOverrides } from "./overrides.js";
-export { parseDecimalBigInt } from "./values.js";
-export type { ConfigOverrides, ExperimentConfig } from "./types.js";
--- papers/01-modelsmc/program-synthesis-prototype/src/shell/scoring/errors.ts
+++
@@ -1,6 +0,0 @@
-export class CoreInvariantError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = "CoreInvariantError";
-  }
-}
